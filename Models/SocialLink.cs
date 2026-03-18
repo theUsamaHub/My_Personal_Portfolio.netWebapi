7ec99bd1f420012c485e5abc9dc9a5069db0021d
usama34102 <u641332@gmail.com>
2026-03-18 18:27:56 +0500

    created sociallinks.cs in models

---
+++ Models/SocialLink.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace My_Personal_Portfolio.Models
+{
+    public class SocialLink
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [StringLength(50)]
+        public string Platform { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        [Url]
+        public string Url { get; set; }
+
+        // Visual Representation
+        [StringLength(50)]
+        public string IconName { get; set; }
+
+        [StringLength(50)]
+        public string IconPack { get; set; } = "fontawesome";
+
+        // Display Options
+        [StringLength(50)]
+        public string DisplayName { get; set; }
+
+        public int DisplayOrder { get; set; } = 0;
+
+        public bool IsActive { get; set; } = true;
+
+        public bool IsPrimary { get; set; } = false;
+
+        public bool ShowInFooter { get; set; } = true;
+
+        // Tracking
+        public int ClickCount { get; set; } = 0;
+
+        public DateTime? LastClicked { get; set; }
+
+        // Timestamps
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
