02d2884e379dd2de020ff34de0b7fbba1809e3ce
theUsamaHub <usamabusiness2025@outlook.com>
2026-03-19 21:49:31 +0500

    updated program.cs file for dbcontext

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using My_Personal_Portfolio.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+// ===== DATABASE CONFIGURATION =====
+// Just uncomment the one you need!
 
+// OPTION 1: SQL Server (for local development) - USE THIS NOW
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// OPTION 2: PostgreSQL (for production) - COMMENT THIS OUT FOR NOW
+// builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
