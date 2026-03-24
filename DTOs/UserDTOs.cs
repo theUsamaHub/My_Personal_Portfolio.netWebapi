4065eeabfbbd56d1c2e0330f990300dbf08e2170
theUsamaHub <usamabusiness2025@outlook.com>
2026-03-24 18:00:02 +0500

    updated the dto file and the settings file

--- DTOs/UserDTOs.cs
+++ DTOs/UserDTOs.cs
@@ -197,7 +197,7 @@ namespace My_Personal_Portfolio.DTOs
         [Required(ErrorMessage = "Email is required")]
         [EmailAddress(ErrorMessage = "Invalid email format")]
         public string Email { get; set; }
-
+        
         [Required(ErrorMessage = "Password is required")]
         [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters")]
         public string Password { get; set; }
